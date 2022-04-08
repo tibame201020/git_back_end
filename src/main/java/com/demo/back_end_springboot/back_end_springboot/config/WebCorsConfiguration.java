b96cb4a495e43270bec9b4f0b2244d0e22b144c9
DESKTOP-EKP8F16\EPIC <tibame201020>
2022-04-08 23:03:54 +0800

    add cors

--- src/main/java/com/demo/back_end_springboot/back_end_springboot/config/WebCorsConfiguration.java
+++ src/main/java/com/demo/back_end_springboot/back_end_springboot/config/WebCorsConfiguration.java
@@ -12,7 +12,7 @@ public class WebCorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("https://tibame201020.github.io", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
